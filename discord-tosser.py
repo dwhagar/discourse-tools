e0bae596147e6df1716bddd240f037e75bc957c4
cyclops <david.hagar@gmail.com>
2018-03-21 19:27:16 -0400

    Making things more secure to have on the public Internet, because I can be an absolute moron.

--- discord-tosser.py
+++ discord-tosser.py
@@ -5,42 +5,32 @@ import json
 import subprocess
 from dateutil.parser import parse
 
+if len(sys.argv) > 1:
+    fileName = sys.argv[1]
+else:
+    fileName = 'discord-tosser.json'
+
+cfg = json.load(open(fileName))
+
+base = cfg["base"]
+key = cfg["key"]
+user = cfg["user"]
+
+discordHooks = {}
+discordIDs = {}
+
+for hook in cfg["hook-urls"]:
+    discordHooks[hook["name"]] = hook["url"]
+
+for uid in cfg["ids"]:
+    discordIDs[uid["name"]] = uid["id"]
+
 # Define our URL values and API key / user to use.
 curlCmd = "curl -sX GET "
 curlPost = "curl -s -X POST -d "
 
-base = "https://forum.moltenaether.com"
-key = "b8dce6591ad59c414ef7bd6a03eb0030fc3c7677f63e54dd5b74fd561f6285b8"
-user = "cyclops"
 auth = "?api_key=" + key + "&api_username=" + user
 suffix = ".json" + auth
-
-# Define our Discord Webhook URL's
-discordHooks = {
-    "announce": "https://discordapp.com/api/webhooks/360583927473111040/PaMUhckgEuqIyVAVLAywm1MpXZER0ou8aeJAvFcohNfp72gz_Cabe2wVJsUzs7CQjPTO",
-    "phoenix": "https://discordapp.com/api/webhooks/360589282974498816/W2KdBwaquBDZOvs0fZZLCw09Z4axE2P1kzBg7mE8P9EEkans_EYZ7yY59wDM7w0mq5V7",
-    "requests": "https://discordapp.com/api/webhooks/360594033833148416/DiF685mPHGewp1Si8CuszwEWEG58EmYnAS8_IYUMeD2W3faHkEXNzyTevugRRHiEqxWx",
-    "bu": "https://discordapp.com/api/webhooks/361246798922121219/VwSpqaUopNXdlwbapDJXiMjmcubjaL-5423TYQ05oY3f6uUbXuPe3sguW3rVO_cXz2KH",
-    "as": "https://discordapp.com/api/webhooks/361248219105329154/0HMaJ_tZLEf7V5zFR0qBhAbiKia0Flf0bSE_it0JMcmcRvhBO1vX3IFCT-2Nx4kUA5eQ",
-    "eos": "https://discordapp.com/api/webhooks/361248777404940288/kAy20j6SdIoYzpbfDNEi1AhKWtoHaCU_xdyavKxafSUxLTO8xPFMRU3nP306PUkEitte",
-    "burning": "https://discordapp.com/api/webhooks/361260772573315072/6QFGMcJG1pQ1Rm49zvZx72M57sgmYndBguc9SKg4tEO-9GOcV_IdmgbyhcFY5nRIxn1H",
-    "rants": "https://discordapp.com/api/webhooks/373667490255077378/bqobZDF976tyWhqxn8jSmnmX9oolsbYBwaRrVd9cutb1TbONqr6qBY9yb_pzzcQkM8Kr",
-    "media": "	https://discordapp.com/api/webhooks/373991743596789761/cKPNwuvUCZhUPeKaydkJshqhOfiR1HSBC8cOAbkNUJun11rnJam19kPrKuSt9x7EoOME",
-    "unlikely": "https://discordapp.com/api/webhooks/377213149205626885/OQse51I970avHYxkzxLx86a4oRMAHQlcNJDpjp5z-Xa8-FL4izCKGAScIZkHmsp6_qDh",
-    "founders": "https://discordapp.com/api/webhooks/408468333113901067/NdCwrCfvbvYbbggPZXqXO3BvS1bpvhxOuRaLlEZglf28pwUBqm32KuQvtNieDaQ8UZAY",
-    "admin": "https://discordapp.com/api/webhooks/425469745463164929/efLpccqfO8M8hEt9UugDGHZt-oNtTb2AxlOH4OBUSgc2vnHtGoWZhBPwjZvhaQkRHZ2b"
-}
-
-# Define some group ID's
-discordIDs = {
-    "creative": "<@&367415080121532417>",
-    "looking": "<@&363809658529644546>",
-    "staff": "<@&244280246793142272>",
-    "players": "<@&243867690068869120>",
-    "eos": "<@&360905173612101632>",
-    "bu": "<@&360904957932732417>",
-    "as": "<@&360905351849181187>"
-}
 
 
 def getJSON(curlString):
