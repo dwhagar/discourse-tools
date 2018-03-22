fd344c71c942ad78d20d565a62567be0cbb54339
cyclops <david.hagar@gmail.com>
2018-03-22 13:55:00 -0400

    Just tweaking the messages a bit.

--- discourse-notifier.py
+++ discourse-notifier.py
@@ -150,7 +150,7 @@ def main():
 
                 if not (push["message"] is None):
                     push["message"] = html.unescape(push["message"])
-                    push["title"] = html.unescape(siteName + " : " + notice["data"]["topic_title"])
+                    push["title"] = html.unescape(siteName + " Update")
                     push["url"] = base + "/t/" + notice["slug"] + "/" + str(notice["topic_id"]) + "/" + str(notice["post_number"])
                     push["url_title"] = html.unescape(notice["data"]["topic_title"])
                     push["token"] = user["apiKey"]
